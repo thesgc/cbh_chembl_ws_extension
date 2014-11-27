0a495a4343b3da5180c58ade5ed5613fae7ddb72
Strets123 <strets123@gmail.com>
2014-11-27 17:26:44 +0000

    changes

---
+++ cbh_chembl_ws_extension/tests.py
@@ -0,0 +1,18 @@
+#Tests to implement
+
+'''
+ModelResources will be created for each of the required models
+
+Therefore we will need to creaste tests for:
+
+CBHCompoundResource - will need to check for permissions via batches
+
+MoleculeBatchResource - which will also update compounds and molecules and also create and update permissions on batchs
+
+Project??
+
+GroupResource
+
+UserResource
+
+'''
