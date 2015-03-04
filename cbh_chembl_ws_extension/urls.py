ba592e48e61599da3d878cbba09f98dea3dcff7f
paulbarrett79 <paulbarrett79@gmail.com>
2015-03-04 16:03:35 +0000

    Logout functionality

--- cbh_chembl_ws_extension/urls.py
+++ cbh_chembl_ws_extension/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url, include
 from cbh_chembl_ws_extension import api as webservices
-from cbh_chembl_ws_extension import Login
+from cbh_chembl_ws_extension import Login, Logout
 from chembl_webservices import __version__ as ws_version
 from chembl_webservices import api_name
 from chembl_core_db.utils import DirectTemplateView
@@ -13,6 +13,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^%s/login' % api_name ,Login.as_view(), name="login"),
+    url(r'^%s/logout' % api_name ,Logout.as_view(), name="logout"),
     url(r'^%s/flow/' % api_name, include(flow)), #adding this to allow configured upload URL within django-flowjs
     url(r'^%s/admin/' % api_name, include(admin.site.urls)),
 	url(r'^grappelli/', include('grappelli.urls')),
