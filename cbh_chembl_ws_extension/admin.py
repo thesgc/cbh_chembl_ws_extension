7eded80624cdf7ad8319723a2852429052b03a5c
Strets123 <strets123@gmail.com>
2015-05-08 09:57:15 +0100

    admin site update

--- cbh_chembl_ws_extension/admin.py
+++ cbh_chembl_ws_extension/admin.py
@@ -80,7 +80,7 @@ class CustomFieldConfigAdmin(ModelAdmin):
         super(CustomFieldConfigAdmin, self).log_change(request, object, message)
         cfr = ProjectResource()
         if object.__class__.__name__ == "CustomFieldConfig":
-            schemaform = json.dumps(cfr.get_schema_form(object))
+            schemaform = json.dumps(cfr.get_schema_form(object,"" ))
             object.schemaform = schemaform
             object.save()
 
