cdee56ecaae9153335ddc1d74c6e11fbbfd3da97
Strets123 <strets123@gmail.com>
2016-02-04 11:41:31 +0000

    date detect off

--- cbh_chembl_ws_extension/elasticsearch_client.py
+++ cbh_chembl_ws_extension/elasticsearch_client.py
@@ -236,6 +236,7 @@ def create_temporary_index(batches, request, index_name):
                     "string_fields": {
                         "match": "*",
                         "match_mapping_type": "string",
+                      "date_detection": False,
                         "mapping": {
                             "type": "string", "store": "no", "index_options": "docs", "index": "analyzed", "omit_norms": True,
                             "fields": {
